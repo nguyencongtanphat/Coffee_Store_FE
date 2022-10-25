1b49c0996a10c0d61438e649e4a6e3aec5047e7f
trunng <20520334@gm.uit.edu.vn>
2022-10-25 22:10:14 +0700

    config font

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css";
+import './assests/fonts/Lexend/Lexend-VariableFont_wght.ttf'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
