21a71ebbe7a764924a69118db7b2490296b43435
trunng <20520334@gm.uit.edu.vn>
2022-10-25 23:20:50 +0700

    configed font family and font style

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css";
 import './assests/fonts/Lexend/Lexend-VariableFont_wght.ttf'
+import './assests/fonts/Ms_Madi/MsMadi-Regular.ttf'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
