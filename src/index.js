167a954f883df06ced201b6edfee2f23f63865bd
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2022-10-26 17:29:16 +0700

    config basic react router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import App from './App';
 import "./index.css";
 import './assests/fonts/Lexend/Lexend-VariableFont_wght.ttf'
 import './assests/fonts/Ms_Madi/MsMadi-Regular.ttf'
 import './tailwind.css'
+import CartPage from './modules/cartPage/screens/CartPage';
+import HomePage from './modules/homePage/screens/HomePage';
+import ErrorPage from './modules/errorPage/screens/ErrorPage';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "cart",
+    element: <CartPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
