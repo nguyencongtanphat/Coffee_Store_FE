7914f28100f0fbaa86821eed1ce9e2945f724855
trunng <20520334@gm.uit.edu.vn>
2022-10-26 08:26:47 +0700

    start deving confirm page

---
+++ src/modules/confirmPage/components/Bill.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Item from './Item'
+
+export default function Bill() {
+  return (
+    <div>
+      <Item/>
+    </div>
+  )
+}
---
+++ src/modules/confirmPage/components/Item.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Item() {
+  return (
+    <div>
+        <p className='text-b10 text-grey200'>1 x Cà Phê Mouse Kem Cheese</p>
+    </div>
+  )
+}
