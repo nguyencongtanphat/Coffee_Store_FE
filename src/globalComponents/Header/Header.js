4eab41caafe2a0668c854ccddc452500f4baee4c
NBNgoc-2k1 <58975779+NBNgoc-2k1@users.noreply.github.com>
2022-10-27 16:18:08 +0700

    finish Header component, add some route for router, add global images

---
+++ src/globalComponents/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavigationBar from './NavigationBar'
+import InfoContactBar from './InfoContactBar'
+const Header = () => {
+    return (
+        <div>
+            <InfoContactBar />
+            <NavigationBar />
+        </div>
+    )
+}
+
+export default Header
