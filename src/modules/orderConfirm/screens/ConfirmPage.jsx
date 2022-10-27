68d8994b062027277d678f402e7bb71ba0fe2612
trunng <20520334@gm.uit.edu.vn>
2022-10-27 23:43:01 +0700

    finish fixing

--- src/modules/orderConfirm/screens/ConfirmPage.jsx
+++ src/modules/orderConfirm/screens/ConfirmPage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import AppButton from '../../../globalComponents/AppButton'
 import Bill from '../components/Bill'
 import CustomerInfo from '../components/CustomerInfo'
+import PageTitle from '../../../globalComponents/PageTitle'
 
-export default function confirmPage() {
+export default function ConfirmPage() {
   return (
-    <div class='md:flex md:ml-10 md:m-16'>
-      <div class="md:flex-initial md:w-[35%] md:p-10 md:h-full">
-        <CustomerInfo/>
-      </div>
-      <div class="relative md:flex-initial md:h-full md:w-6/12 md:ml-10">
-        <Bill/>
-        <div className='absolute left-0 right-0 ml-auto mr-auto text-center mt-[-35px] md:left-14'>
-            <button class="bg-orange hover:bg-orange text-white font-bold py-2.5 px-12 rounded-lg border-none text-b13 md:text-b11 md:py-3 md:px-20 md:rounded-xl">Đặt hàng</button>   
+    <div className='flex flex-col align-center'>
+      <PageTitle title='XÁC NHẬN ĐƠN HÀNG' class='justify-center'/>
+      <div class='md:flex md:ml-10 md:m-16'>
+        <div class="md:flex-initial md:w-[35%] md:p-10 md:h-full">
+          <CustomerInfo/>
+        </div>
+        <div class="relative md:flex-initial md:h-full md:w-6/12 md:ml-10 flex flex-col justify-center">
+          <Bill/>
+          <div className='flex justify-center md:ml-16 mt-0'>
+            <AppButton text='Đặt hàng'/>
+          </div>
         </div>
       </div>
     </div>
