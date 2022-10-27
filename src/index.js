7980f2367c5f139c88cf0d48121c7146f8b94180
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2022-10-27 21:26:03 +0700

    add some new code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import App from './App';
 import "./index.css";
 import './assests/fonts/Lexend/Lexend-VariableFont_wght.ttf'
 import './assests/fonts/Ms_Madi/MsMadi-Regular.ttf'
--- src/modules/cartPage/components/Table.jsx
+++ src/modules/cartPage/components/Table.jsx
@@ -1,87 +1,92 @@
-import React from 'react'
-import Item from './Item'
+import React from "react";
+import Item from "./Item";
 
 function Table() {
-    const orderList = [
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-      {
-        name: "Cafe chon",
-        size: "L",
-        number: "2",
-        price: "16.000",
-      },
-    ];
+  const orderList = [
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+    {
+      name: "Cafe chon",
+      size: "L",
+      number: "2",
+      price: "16.000",
+    },
+  ];
   return (
-    <div className='w-4/5 md:w-6/12'>
+    <div className="w-4/5 md:w-3/5">
       <ul className="list-none p-0 max-h-[70vh] overflow-auto">
         {orderList.map((item) => (
           <li className="my-3">
-            <Item />
+            <Item
+              name={item.name}
+              size={item.size}
+              number={item.number}
+              price={item.price}
+            />
           </li>
         ))}
       </ul>
@@ -89,4 +94,4 @@ function Table() {
   );
 }
 
-export default Table
+export default Table;
