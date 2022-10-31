a8433c873c9cde02136dc544ae976ee5019bd3d0
NBNgoc-2k1 <58975779+NBNgoc-2k1@users.noreply.github.com>
2022-10-31 15:04:04 +0700

    Squashed commit of the following:
    commit 70c851775dea7703415db1ff11ef449cde34907c
    Merge: ff2ecf8 aa00637
    Author: Nguyen Cong Tan Phat <79776627+nguyencongtanphat@users.noreply.github.com>
    Date:   Mon Oct 31 15:00:22 2022 +0700
    
        Merge pull request #13 from nguyencongtanphat/feat/footer
    
        Feat/footer
    
    commit aa006372ccac646e0e6bd76898fa43f1b4d0f250
    Author: sotnk21 <20520225@gm.uit.edu.vn>
    Date:   Mon Oct 31 14:47:37 2022 +0700
    
        update flex
    
    commit 7cac5cfe7f66e294bd814f58b696b9881e49750e
    Author: sotnk21 <20520225@gm.uit.edu.vn>
    Date:   Mon Oct 31 00:36:08 2022 +0700
    
        feat/footer
    
    commit 9f6493f01d211e0bb1dc1fbd63a89eda8edf9518
    Merge: 554c753 ff2ecf8
    Author: sotnk21 <20520225@gm.uit.edu.vn>
    Date:   Sun Oct 30 22:12:32 2022 +0700
    
        footer conflict
    
    commit 554c753f47097991a5a90ae93b18cced802958af
    Author: sotnk21 <20520225@gm.uit.edu.vn>
    Date:   Sun Oct 30 22:08:21 2022 +0700
    
        footer
    
    commit ff2ecf8f9bda20a6e94784c1052f247a6f6a2117
    Merge: 83ecba4 bd229f9
    Author: Nguyen Cong Tan Phat <79776627+nguyencongtanphat@users.noreply.github.com>
    Date:   Sun Oct 30 16:33:52 2022 +0700
    
        Merge pull request #12 from nguyencongtanphat/feat/errorPage
    
        finished code of error page
    
    commit bd229f9b73aefbc32723ab324d4e7986b8a69907
    Author: Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
    Date:   Sun Oct 30 16:32:43 2022 +0700
    
        modify error Page
    
    commit e1221fee309f93f88a81f68b8c39b76a557ed6ee
    Author: Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
    Date:   Sun Oct 30 16:16:05 2022 +0700
    
        test
    
    commit 52680d2ba1b5da9eb6249684ea365be0d30cf0d6
    Merge: 19ab565 83ecba4
    Author: trunng <20520334@gm.uit.edu.vn>
    Date:   Sun Oct 30 00:09:12 2022 +0700
    
        finished error page
    
    commit 19ab5651966416ab961e87dcef4b2268a0443f7a
    Author: trunng <20520334@gm.uit.edu.vn>
    Date:   Sat Oct 29 23:55:14 2022 +0700
    
        finished code error page
    
    commit 83ecba4d03a1a84921063c329586174e368eae07
    Merge: ca64ec2 fcc787d
    Author: Nguyen Cong Tan Phat <79776627+nguyencongtanphat@users.noreply.github.com>
    Date:   Sat Oct 29 22:23:37 2022 +0700
    
        Merge pull request #11 from nguyencongtanphat/global_component
    
        Finish Global-component ProductCard
    
    commit fcc787dfb7e145785a95bb0dcc7ea203e18959d2
    Author: Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
    Date:   Sat Oct 29 22:22:59 2022 +0700
    
        add new code for app button
    
    commit 2652daadc8c9e8f8884ba010bdcaebc21abf98f3
    Author: NBNgoc-2k1 <58975779+NBNgoc-2k1@users.noreply.github.com>
    Date:   Sat Oct 29 21:09:16 2022 +0700
    
        Update logo for app
    
    commit f71f830531f811848a55c2b26008d88b1143c7fc
    Author: VTPie <91325735+VTPie@users.noreply.github.com>
    Date:   Sat Oct 29 20:58:16 2022 +0700
    
        Demo ProductCard
    
    commit 15420ff86043f51c665fff317057557656bc4d1e
    Merge: 3cd2804 ca64ec2
    Author: sotnk21 <20520225@gm.uit.edu.vn>
    Date:   Sat Oct 29 17:12:37 2022 +0700
    
        init
    
    commit 3cd280495dde214b66e3c9adc21db4810313234e
    Author: sotnk21 <20520225@gm.uit.edu.vn>
    Date:   Sat Oct 29 17:10:07 2022 +0700
    
        init
    
    commit 26857a6fe540724b569959557fadf18074cc8d71
    Author: VTPie <91325735+VTPie@users.noreply.github.com>
    Date:   Fri Oct 28 21:17:38 2022 +0700
    
        Finish Global-component ProductCard
    
        there are 4 properties
        - avtSrc: images of product (string)
        - name: product name (string)
        - category: category (string)
        - price: price (number)
    
    commit 15356f260164e4945d071c5a381790f30d6d5f87
    Merge: 4dafcc3 bc95b61
    Author: Nguyen Cong Tan Phat <79776627+nguyencongtanphat@users.noreply.github.com>
    Date:   Fri Oct 28 17:21:03 2022 +0700
    
        Merge pull request #10 from nguyencongtanphat/feat/orderConfirm
    
        Feat/order confirm
    
    commit bc95b61961a336b29e651056a7805ea91f9644fe
    Author: Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
    Date:   Fri Oct 28 17:19:41 2022 +0700
    
        complete edit confirm page

--- src/globalComponents/AppButton.jsx
+++ src/globalComponents/AppButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 function AppButton({ icon, text, onClick, className }) {
   
   return (
     <button
       className={`flex bg-orange border-none px-4 justify-center
                 py-3 gap-3 rounded-xl
-                md:py-5 md:px-8 ${className}`}
+                md:py-5 md:px-8 ${className} `}
       onClick={onClick}
     >
       <div className="text-white text-12 
@@ -14,5 +14,16 @@ function AppButton({ icon, text, onClick, className }) {
     </button>
   );
 }
+
+AppButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  className:PropTypes.string,
+  icon: PropTypes.any,
+};
+
+AppButton.defaultProps = {
+  text: "default text",
+};
 
 export default AppButton;
--- src/globalComponents/PageTitle.jsx
+++ src/globalComponents/PageTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PageTitle({ title, className }) {
   const titlePage = (title ?? "Default Page Title").toUpperCase();
   return (
-    <div className={`flex items-center m-2 ${className}`}>
+    <div className={`flex justify-center items-center m-2 ${className}`}>
       <img
         src="https://img.freepik.com/free-vector/start_53876-25533.jpg"
         alt=""
