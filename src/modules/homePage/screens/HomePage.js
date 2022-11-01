0ee90391f42a1376a5dbcf52fc97fe801671c423
NBNgoc-2k1 <58975779+NBNgoc-2k1@users.noreply.github.com>
2022-11-01 17:03:15 +0700

    Finish Homepage

--- src/modules/homePage/screens/HomePage.js
+++ src/modules/homePage/screens/HomePage.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import BlogSummary from '../../../globalComponents/BlogSummary'
-import sample_thumbnail from '../../../assests/images/global/sample_thumbnail_blog.png'
-import ProductCard from '../../../globalComponents/ProductCard.js'
-import product_sample from '../../../assests/images/global/product_sample.png'
+import Banner from '../components/Banner'
+import NewProduct from '../components/NewProduct'
+import BlogSlider from '../components/BlogSlider'
+import BestSeller from '../components/BestSeller'
 function HomePage() {
   return (
     <div>
-      <ProductCard avtSrc={product_sample} name="coffee" category="coffee" price="200"></ProductCard>
+      <Banner />
+      <BestSeller />
+      <NewProduct />
+      <BlogSlider />
     </div>
   )
 }
