5ba9b5d08e2f5c05c255edbf4a4c95c4b1734dd3
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2022-11-07 22:56:53 +0700

    config context and popup modal

--- src/Root.js
+++ src/Root.js
@@ -3,16 +3,18 @@ import { Outlet } from 'react-router'
 import Header from './globalComponents/Header/Header'
 import Footer from './globalComponents/Footer/Footer'
 
+
 const Root = () => {
+    
     return (
-        <div>
-            <Header />
-            <div id="detail">
-                <Outlet />
-            </div>
-            <Footer />
+      <div>
+        <Header />
+        <div id="detail">
+          <Outlet />
         </div>
-    )
+        <Footer />
+      </div>
+    );
 }
 
 export default Root
