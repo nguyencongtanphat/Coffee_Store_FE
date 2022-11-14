0ee3ce75a59bbac029d3432bd1fdcb1b07b96cb5
trunng <20520334@gm.uit.edu.vn>
2022-11-14 15:38:03 +0700

    finished logic code for 2 Cases of Confirm page

--- src/modules/orderConfirm/screens/ConfirmPage.jsx
+++ src/modules/orderConfirm/screens/ConfirmPage.jsx
@@ -16,32 +16,41 @@ export default function ConfirmPage() {
   const [appState, dispatch] = useContext(UserContext);
   console.log("dataInfo", appState);
   
-  return (
-    <div className="flex flex-col align-center">
-      <PageTitle title="XÁC NHẬN ĐƠN HÀNG" class="justify-center w-fit" />
-      <div class="md:flex md:ml-10 md:m-4 mt-0">
-        <div class="md:flex-initial md:w-[35%] md:p-10 md:h-full md:mt-0">
-          <CustomerInfo name={appState.fullName} phone={appState.phoneNumber} address={appState.address[0].Value}/>
-        </div>
-        <div class="md:flex-initial md:h-full md:w-6/12 md:ml-10 flex flex-col justify-center items-center">
-          <Bill class=" md:pl-10 md:ml-20" cat={listProducts}/>
+  return (appState.isLogin) ? 
+    ( (location.state!=undefined) ? 
+      (
+        <div className="flex flex-col align-center">
+          <PageTitle title="XÁC NHẬN ĐƠN HÀNG" class="justify-center w-fit" />
+          <div class="md:flex md:ml-10 md:m-4 mt-0">
+            <div class="md:flex-initial md:w-[35%] md:p-10 md:h-full md:mt-0">
+              <CustomerInfo name={appState.fullName} phone={appState.phoneNumber} address={appState.address[0].Value}/>
+            </div>
+            <div class="md:flex-initial md:h-full md:w-6/12 md:ml-10 flex flex-col justify-center items-center">
+              <Bill class=" md:pl-10 md:ml-20" cat={listProducts}/>
 
-          <AppButton
-            text="Đặt hàng"
-            className="w-fit "
+              <AppButton
+                text="Đặt hàng"
+                className="w-fit "
+              />
+            </div>
+          </div>
+          <img
+            src={right}
+            alt=""
+            className="absolute hidden top-[70px] right-0 -z-10 md:block md:w-[200px] lg:w-[230px]"
+          />
+          <img
+            src={left}
+            alt=""
+            className="absolute hidden bottom-[-200px]  -z-10 md:block md:w-[250px] lg:w-[300px] "
           />
         </div>
-      </div>
-      <img
-        src={right}
-        alt=""
-        className="absolute hidden top-[70px] right-0 -z-10 md:block md:w-[200px] lg:w-[230px]"
-      />
-      <img
-        src={left}
-        alt=""
-        className="absolute hidden bottom-[-200px]  -z-10 md:block md:w-[250px] lg:w-[300px] "
-      />
-    </div>
-  );
+      )
+      : (
+        <h1 className="text-center">Bạn cần phải chọn ít nhất một sản phẩm trong Giỏ hàng để Xác nhận đơn hàng!</h1>
+      )
+    )
+    : ( 
+      <h1 className="text-center">Bạn cần phải Đăng nhập để Xác nhận đơn hàng!</h1>
+    );
 }
