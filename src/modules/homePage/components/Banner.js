2eb3f5ac1aaa7bbbbc41a35d264e2104bb36b70a
Ngân Thanh <20520250@gm.uit.edu.vn>
2022-11-15 19:52:44 +0700

    finish banner

--- src/modules/homePage/components/Banner.js
+++ src/modules/homePage/components/Banner.js
@@ -4,11 +4,19 @@ import { Autoplay, Pagination } from 'swiper'
 import "swiper/css/autoplay"
 import "swiper/css/pagination";
 import "swiper/css";
-import './slider_style.css'
+import './slider_style.css';
+import banner1 from "d:/COFFEE/Coffee_Store_FE/src/assests/images/home/banner/banner1.png";
+import banner2 from "d:/COFFEE/Coffee_Store_FE/src/assests/images/home/banner/banner2.png";
+import banner3 from "d:/COFFEE/Coffee_Store_FE/src/assests/images/home/banner/banner3.png";
+import banner4 from "d:/COFFEE/Coffee_Store_FE/src/assests/images/home/banner/banner4.png";
+import banner1_mb from "d:/COFFEE/Coffee_Store_FE/src/assests/images/home/banner/banner1_mb.png";
+import banner2_mb from "d:/COFFEE/Coffee_Store_FE/src/assests/images/home/banner/banner2_mb.png";
+import banner3_mb from "d:/COFFEE/Coffee_Store_FE/src/assests/images/home/banner/banner3_mb.png";
+import banner4_mb from "d:/COFFEE/Coffee_Store_FE/src/assests/images/home/banner/banner4_mb.png";
 
 const Banner = () => {
     return (
-        <div className="w-full h-64 bg-orange lg:h-96">
+        <div className="w-full h-auto bg-white">
             <Swiper
                 pagination={true}
                 autoplay={{
@@ -16,14 +24,62 @@ const Banner = () => {
                 }}
                 modules={[Autoplay, Pagination]}
             >
-                <SwiperSlide>
-
+                <SwiperSlide className="md:hidden">
+                <img
+                    src={banner1_mb}
+                    alt=""
+                    className="w-full h-auto"
+                />
+                </SwiperSlide> 
+                <SwiperSlide className="md:hidden">
+                <img
+                    src={banner2_mb}
+                    alt=""
+                    className="w-full h-auto"
+                />
                 </SwiperSlide>
-                <SwiperSlide>
-
+                <SwiperSlide className="md:hidden">
+                <img
+                    src={banner3_mb}
+                    alt=""
+                    className="w-full h-auto"
+                />
                 </SwiperSlide>
-                <SwiperSlide>
-
+                <SwiperSlide className="md:hidden">
+                <img
+                    src={banner4_mb}
+                    alt=""
+                    className="w-full h-auto"
+                />
+                </SwiperSlide>
+                
+                <SwiperSlide className="hidden md:flex">
+                <img
+                    src={banner1}
+                    alt=""
+                    className="w-full h-auto"
+                />
+                </SwiperSlide>
+                <SwiperSlide className="hidden md:flex">
+                <img
+                    src={banner2}
+                    alt=""
+                    className="w-full h-auto"
+                />
+                </SwiperSlide>
+                <SwiperSlide className="hidden md:flex">
+                <img
+                    src={banner3}
+                    alt=""
+                    className="w-full h-auto"
+                />
+                </SwiperSlide>
+                <SwiperSlide className="hidden md:flex">
+                <img
+                    src={banner4}
+                    alt=""
+                    className="w-full h-auto"
+                />
                 </SwiperSlide>
             </Swiper>
         </div>
