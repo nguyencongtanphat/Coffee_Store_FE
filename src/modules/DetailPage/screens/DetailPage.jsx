a0674649b9db97e89c248a6c667b6e42de771719
unknown <nguyenhoanghuy4622@gmail.com>
2022-11-20 23:30:37 +0700

    fix UI error

--- src/modules/DetailPage/screens/DetailPage.jsx
+++ src/modules/DetailPage/screens/DetailPage.jsx
@@ -101,68 +101,67 @@ function DetailPage() {
               />
             </div>
           </div>
-          <div className="md:ml-[30px] flex-col flex justify-center items-center">
-            <div className="w-[283px] h-auto md:w-[480px] ">
-              <p className="mt-[20px] md:mt-0 text-brown text-b5 md:text-b3 lg:text-b1">
-                {dtInfo.Name || "Tên sản phẩm"}
-              </p>
-              <p className="mt-[10px] text-orange text-b7 md:text-b5 lg:text-b3">
-                {FormatterService.format(price)}
-              </p>
-              <p className="mt-[10px] text-gray-500 text-b13 md:text-b11 lg:text-b9">
-                CloudFee Creme Brulee Caramel ngon khó cưỡng bởi lớp kem trứng
-                Creme Brulee bồng bềnh béo mịn, ngọt thanh của Caramel, thêm xíu
-                đắng nhẹ từ cà phê, kèm topping thạch cà phê dai dai giòn giòn.{" "}
-              </p>
-            </div>
-            <div className="mt-[10px] flex flex-row w-[425px] h-auto items-center">
-              <p className="text-b11 md:text-b10 lg:text-b9">Size:</p>
-              <AppButton2
-                className={`ml-[10p`}
-                isActive={size === "Small"}
-                text="S"
-                onClick={changeSize.bind(null, "Small")}
-              />
-              <AppButton2
-                className="ml-[10px]"
-                isActive={size === "Medium"}
-                text="M"
-                onClick={changeSize.bind(null, "Medium")}
-              />
-              <AppButton2
-                className="ml-[10px]"
-                isActive={size === "Large"}
-                text="L"
-                onClick={changeSize.bind(null, "Large")}
-              />
-            </div>
-
-            <did className="flex flex-row mt-[10px] items-center">
-              <p className="text-b11 md:text-b10 lg:text-b9">Số lượng:</p>
-              <p
-                className="text-b11 md:text-b10 lg:text-b10 ml-[10px] text-gray-400 cursor-pointer"
-                onClick={changeNumber.bind(null, "minus")}
-              >
-                -
-              </p>
-              <AppButton2 className="ml-[10px]" text={number} />
-              <p
-                className="text-b11 md:text-b10 lg:text-b10 ml-[10px] text-gray-400 cursor-pointer"
-                onClick={changeNumber.bind(null, "add")}
-              >
-                +
-              </p>
-            </did>
-            <div className="flex flex-col md:flex-row">
-              <AppButton
-                className="mt-[15px] items-center w-[300px]"
-                text="Thêm vào giỏ hàng"
-                onClick={addProductToCartHandler}
-              />
-              <AppButton
-                className="md:ml-[10px] mt-[15px] items-center w-[300px]"
-                text="Đặt hàng ngay"
-              />
+            <div className="md:ml-[30px] flex-col flex justify-center items-center">
+              <div className="w-[283px] h-auto md:w-[480px] ">
+                <p className="mt-[20px] md:mt-0 text-brown text-b5 md:text-b3 lg:text-b1">
+                  {dtInfo.Name || "Tên sản phẩm"}
+                </p>
+                <p className="mt-[10px] text-orange text-b7 md:text-b5 lg:text-b3">
+                  {FormatterService.format(price)}
+                </p>
+                <p className="mt-[10px] text-gray-500 text-b13 md:text-b11 lg:text-b9">
+                  CloudFee Creme Brulee Caramel ngon khó cưỡng bởi lớp kem trứng
+                  Creme Brulee bồng bềnh béo mịn, ngọt thanh của Caramel, thêm xíu
+                  đắng nhẹ từ cà phê, kèm topping thạch cà phê dai dai giòn giòn.{" "}
+                </p>
+                <div className="mt-[10px] flex flex-row w-[425px] h-auto items-center">
+                  <p className="text-b11 md:text-b10 lg:text-b9">Size:</p>
+                  <AppButton2
+                    className="ml-[10px]"
+                    isActive={size === "Small"}
+                    text="S"
+                    onClick={changeSize.bind(null, "Small")}
+                  />
+                  <AppButton2
+                    className="ml-[10px] md:ml-[20px]"
+                    isActive={size === "Medium"}
+                    text="M"
+                    onClick={changeSize.bind(null, "Medium")}
+                  />
+                  <AppButton2
+                    className="ml-[10px] md:ml-[20px]"
+                    isActive={size === "Large"}
+                    text="L"
+                    onClick={changeSize.bind(null, "Large")}
+                  />
+                </div>
+                <did className="flex flex-row mt-[10px] items-center">
+                  <p className="text-b11 md:text-b10 lg:text-b9">Số lượng:</p>
+                  <p
+                    className="text-b11 md:text-b10 lg:text-b10 ml-[10px] text-gray-400 cursor-pointer"
+                    onClick={changeNumber.bind(null, "minus")}
+                  >
+                    -
+                  </p>
+                  <AppButton2 className="ml-[10px]" text={number} />
+                  <p
+                    className="text-b11 md:text-b10 lg:text-b10 ml-[10px] text-gray-400 cursor-pointer"
+                    onClick={changeNumber.bind(null, "add")}
+                  >
+                    +
+                  </p>
+                </did>
+                <div className="flex flex-col md:flex-row">
+                  <AppButton
+                    className="mt-[15px] items-center w-auto"
+                    text="Thêm vào giỏ hàng"
+                    onClick={addProductToCartHandler}
+                  />
+                  <AppButton
+                    className="md:ml-[20px] mt-[15px] items-center w-auto"
+                    text="Đặt hàng ngay"
+                  />
+              </div>
             </div>
           </div>
         </div>
