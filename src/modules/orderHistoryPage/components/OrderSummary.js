f6c76f818955ba9cc95918689d080163366be3fb
NBNgoc-2k1 <58975779+NBNgoc-2k1@users.noreply.github.com>
2022-11-20 09:58:40 +0700

    Finish Order History page

--- src/modules/orderHistoryPage/components/OrderSummary.js
+++ src/modules/orderHistoryPage/components/OrderSummary.js
@@ -1,50 +1,31 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import { FormatterService } from '../../../service'
+import OrderItemSummary from './OrderItemSummary'
 import sample from './Rectangle 4065.png'
-const OrderSummary = () => {
+const OrderSummary = (props) => {
     return (
         <div className="pb-4 mb-4 lg:pb-8 lg:mb-8 lg:w-2/3 lg:flex"
             style={{borderBottom: '1px solid #E0E0E0'}}
         >
             <div className="flex lg:flex-col">
-                <p className="text-b5 md:text-b3 text-orange">#01</p>
+                <p className="text-b5 md:text-b3 text-orange">#0{props.index + 1}</p>
                 <p className="text-b11 md:text-b9 text-orange flex my-2 mx-4 lg:mx-0">
-                    <p className="text-black mr-2">Ngày:</p> 25/10/2022
+                    <p className="text-black mr-2">Ngày:</p> {props.dateOrder}
                 </p>
                 <p className="text-b11 md:text-b9 text-orange flex my-2">
-                    <p className="text-black mr-2">Tổng tiền:</p> 159000d
+                    <p className="text-black mr-2">Tổng tiền:</p> {FormatterService.format(props.orderInfo.TotalAmount)}
                 </p>
             </div>
             <div className="flex ml-16">
                 <img src={sample} className="lg:w-28 lg:h-28"/>
                 <div className="ml-4">
-                    <div className="flex items-center w-48 lg:w-72 justify-between text-grey200">
-                        <p className="text-b13 md:text-b9">
-                            Cà Phê Mouse Kem Cheese
-                        </p>
-                        <p className="text-b13 md:text-b9">
-                            x2
-                        </p>
-
-                    </div>
-                    <div className="flex items-center w-48 lg:w-72 justify-between text-grey200">
-                        <p className="text-b13 md:text-b9">
-                            Trà Long Nhãn Hạt Sen
-                        </p>
-                        <p className="text-b13 md:text-b9">
-                            x2
-                        </p>
-
-                    </div>
-                    <div className="flex items-center w-48 lg:w-72 justify-between text-grey200 lg:mb-4">
-                        <p className="text-b13 md:text-b9">
-                            Cà Phê Sữa Đá
-                        </p>
-                        <p className="text-b13 md:text-b9">
-                            x2
-                        </p>
-
-                    </div>
+                    {
+                        props.orderInfo.DetailOrder.map(orderProduct => <OrderItemSummary 
+                            name={orderProduct.Item.Name}
+                            quantity={orderProduct.Quantity}
+                        />)
+                    }
                     <Link className="text-b13 md:text-b9 text-orange">
                         Xem thêm
                     </Link>
--- src/modules/orderHistoryPage/screens/OrderHistory.js
+++ src/modules/orderHistoryPage/screens/OrderHistory.js
@@ -9,27 +9,42 @@ const OrderHistory = () => {
     const [ordersByUser, setOrdersByUser] = useState([])
     const [appState, dispatch] = useContext(UserContext);
 
-    // useEffect(async () => {
-    //     const response = await createAxiosInstance().get(`order/customer/${appState.id}`)
-    //     setOrdersByUser(response.data.Orders)
-    // }, [])
+    const GetOrdersByUser = async () => {
+        const response = await createAxiosInstance().get(
+            `order/customer/${appState.id}`
+        )
+
+        setOrdersByUser(response.data.Orders.reverse());
+    }
+
+    const FormatDate = (date) => {
+        const unFormattedDate = new Date(date);
+        const yyyy = unFormattedDate.getFullYear();
+        let mm = unFormattedDate.getMonth() + 1; // Months start at 0!
+        let dd = unFormattedDate.getDate();
+
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
+
+        return dd + '/' + mm + '/' + yyyy;
+    }
+
+    useEffect(() => {
+        GetOrdersByUser()
+    }, [])
 
     return (
         <div>
             <PageTitle title="LỊCH SỬ MUA HÀNG" className="my-8" />
-            {ordersByUser.length > 0 && <div className="m-4">
+            {ordersByUser.length > 0 && <div className="m-4 lg:mx-96 lg:w-2/3">
                 {
-                    ordersByUser.map(order => <OrderSummary
+                    ordersByUser.map((order, index) => <OrderSummary
+                        index={index}
                         orderInfo={order}
+                        dateOrder={FormatDate(order.createdAt)}
                     />)
                 }
-
             </div>}
-            <div className="m-4 lg:mx-96 lg:w-2/3">
-                <OrderSummary />
-                <OrderSummary />
-                <OrderSummary />
-            </div>
         </div>
     )
 }
