f1f2ad95842928adc3d160e81f88e4599c7778f1
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2022-12-03 09:41:32 +0700

    fix some error in card

--- src/globalComponents/ProductCard.js
+++ src/globalComponents/ProductCard.js
@@ -7,32 +7,34 @@ import styles from "./ProductCard.module.css"
 
 const ProductCard = (props) => {
     return (
-        <div className={`lg:w-[17rem] lg:max-h-[22rem] lg:w-[17rem]
+      <div
+        className={`lg:w-[17rem] lg:max-h-[22rem] overflow-hidden
                 w-[9.5rem] rounded-2xl m-4 bg-white transition-shadow shadow-lg shadow-grey300 hover:bg-gray-100 
-                ${props.className}`}>
-            <div className='overflow-hidden'>
-                <img src={props.avtSrc} alt="This is a product avatar"
-                    className= {`${styles.productImg} w-[8.5rem] my-4 mx-2 max-h-28
-                    lg:m-4 lg:max-h-48 lg:max-w-60 lg:w-60 rounded-2xl`}  
-                />
-            </div>
-            
-            <div className="p-2 lg:p-4">
-                <p className="text-b11 lg:text-b8 text-grey200 h-11"
-                >{props.name}</p>
-                <p className="text-b11 lg:text-b9 text-grey300"
-                >{props.category}</p>
-                <div className="flex justify-between mt-1">
-                    <p className="text-orange text-b9 pt-1 lg:text-b7"
-                    >{FormatterService.format(props.price)}
-                    </p>
-                    <button className="border-none bg-transparent cursor-pointer ">
-                        <FontAwesomeIcon icon={faPlusCircle} color="orange" size="2x"  />
-                    </button>
-                </div>
-            </div>
+                ${props.className}`}
+      >
+        <div className="">
+          <img
+            src={props.avtSrc}
+            alt="This is a product avatar"
+            className={`${styles.productImg} w-[8.5rem] my-4 mx-2 max-h-28
+                    lg:m-4 lg:max-h-48 lg:max-w-60 lg:w-60 rounded-2xl`}
+          />
         </div>
-    )
+
+        <div className="p-2 lg:p-4">
+          <p className="text-b11 lg:text-b8 text-grey200 h-11">{props.name}</p>
+          <p className="text-b11 lg:text-b9 text-grey300">{props.category}</p>
+          <div className="flex justify-between mt-1">
+            <p className="text-orange text-b9 pt-1 lg:text-b7">
+              {FormatterService.format(props.price)}
+            </p>
+            <button className="border-none bg-transparent cursor-pointer ">
+              <FontAwesomeIcon icon={faPlusCircle} color="orange" size="2x" />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 ProductCard.propTypes = {
