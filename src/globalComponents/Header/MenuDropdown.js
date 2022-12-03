535b75479f116729d3c7321b14a2f944629b464c
NBNgoc-2k1 <58975779+NBNgoc-2k1@users.noreply.github.com>
2022-12-03 22:28:50 +0700

    close menu dropdown when click outside

--- src/globalComponents/Header/MenuDropdown.js
+++ src/globalComponents/Header/MenuDropdown.js
@@ -21,59 +21,67 @@ const MenuDropdown = (props) => {
       dispatch(setStateLogOut());
       props.onClose();
     }
-    //console.log(res);
   };
 
   return (
-    <div
-      className={`${
-        props.open ? "flex" : "hidden"
-      } flex-col rounded-lg justify-center items-start w-64
+    <div className={`${props.open ? "flex" : "hidden"
+      } w-full h-screen fixed top-0
+    `}
+      onClick={(e) => {
+        if (e.currentTarget === e.target) {
+          props.onClose();
+        }
+      }}
+    >
+
+      <div
+        className={`flex-col rounded-lg justify-center items-start w-64
             absolute top-28 right-12 opacity-90
         `}
-      style={{ backgroundColor: "#F4D8B2" }}
-    >
-      <span
-        className="absolute right-3 -top-2.5"
-        style={{
-          borderLeft: "10px solid transparent",
-          borderRight: "10px solid transparent",
-          borderBottom: "10px solid #F4D8B2",
-        }}
-      ></span>
-      <div className="flex max-w-full flex-col ml-4">
-        <div className="flex mt-4" onClick={() => props.onClose()}>
-          <FontAwesomeIcon icon={faAddressCard} size="lg" color="black" />
-          <Link
-            className="no-underline ml-4 text-black 
+        style={{ backgroundColor: "#F4D8B2" }}
+      >
+        <span
+          className="absolute right-3 -top-2.5"
+          style={{
+            borderLeft: "10px solid transparent",
+            borderRight: "10px solid transparent",
+            borderBottom: "10px solid #F4D8B2",
+          }}
+        ></span>
+        <div className="flex max-w-full flex-col ml-4">
+          <div className="flex mt-4" onClick={() => props.onClose()}>
+            <FontAwesomeIcon icon={faAddressCard} size="lg" color="black" />
+            <Link
+              className="no-underline ml-4 text-black 
                     hover:underline underline-offset-4 decoration-2"
-            to="account"
-          >
-            Thông tin cá nhân
+              to="account"
+            >
+              Thông tin cá nhân
           </Link>
-        </div>
-        <div className="flex my-4" onClick={() => props.onClose()}>
-          <FontAwesomeIcon icon={faClockRotateLeft} size="lg" color="black" />
-          <Link
-            className="no-underline ml-5 text-black 
+          </div>
+          <div className="flex my-4" onClick={() => props.onClose()}>
+            <FontAwesomeIcon icon={faClockRotateLeft} size="lg" color="black" />
+            <Link
+              className="no-underline ml-5 text-black 
                     hover:underline underline-offset-4 decoration-2"
-            to="history"
-          >
-            Lịch sử mua hàng
+              to="history"
+            >
+              Lịch sử mua hàng
           </Link>
-        </div>
-        <div className="flex mb-4" onClick={logoutHandler}>
-          <FontAwesomeIcon
-            icon={faArrowRightFromBracket}
-            size="lg"
-            color="black"
-          />
-          <Link
-            className="no-underline ml-4 text-black 
+          </div>
+          <div className="flex mb-4" onClick={logoutHandler}>
+            <FontAwesomeIcon
+              icon={faArrowRightFromBracket}
+              size="lg"
+              color="black"
+            />
+            <Link
+              className="no-underline ml-4 text-black 
                     hover:underline underline-offset-4 decoration-2"
-          >
-            Đăng xuất
+            >
+              Đăng xuất
           </Link>
+          </div>
         </div>
       </div>
     </div>
