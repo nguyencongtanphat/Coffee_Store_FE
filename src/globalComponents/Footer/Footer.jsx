6c7258057e0227c307a8dcc37f45836801b45a05
NBNgoc-2k1 <58975779+NBNgoc-2k1@users.noreply.github.com>
2022-12-03 11:26:40 +0700

    add location coffee shop on map on footer

--- src/globalComponents/Footer/Footer.jsx
+++ src/globalComponents/Footer/Footer.jsx
@@ -9,7 +9,7 @@ import { ReactComponent as IconFacebook } from "../../assests/images/svg/faceboo
 import { ReactComponent as IconYoutube } from "../../assests/images/svg/youtube_icon.svg"
 import { ReactComponent as IconInstagram } from "../../assests/images/svg/instagram_icon.svg"
 
-export default function Footer({props}) {
+export default function Footer({ props }) {
     const location = useLocation();
     const [pathname, setPathname] = useState('/')
 
@@ -21,10 +21,10 @@ export default function Footer({props}) {
     return (
         <footer className={
             `w-screen bg-grey h-82 sm:h-96
-            ${(pathname === '/') ? 'bg-white': 'bg-grey'}`
-            }>
+            ${(pathname === '/') ? 'bg-white' : 'bg-grey'}`
+        }>
             <div className="flex flex-row flex-wrap sm:flex-nowrap sm:pl-[50px] p-2">
-                <div className="basis-full sm:basis-4/12 sm:mt-[50px] h-28 "> 
+                <div className="basis-full sm:basis-4/12 sm:mt-[50px] h-28 ">
                     <div className='mx-auto w-min'>
                         <Link to="/">
                             <img src={logo} alt="logo" className=" w-48 sm:w-60" onClick={() => { }}
@@ -33,24 +33,24 @@ export default function Footer({props}) {
                         </Link>
                         <div className=" flex flex-row mx-auto sm:mt-4 justify-between w-[132px]">
                             <a className="basis-1/3 h-max w-max"
-                            href="https://www.facebook.com/">
-                                <BgFacebook className="absolute"/>
-                                <IconFacebook className="relative top-[7px] left-[11.25px]"/>
-                            </a>
-                            <a className="basis-1/3" 
-                            href="https://www.instagram.com/">
-                                <BgInstagram className="absolute"/>
-                                <IconInstagram className="relative top-[6.5px] left-[7.25px]"/>
+                                href="https://www.facebook.com/">
+                                <BgFacebook className="absolute" />
+                                <IconFacebook className="relative top-[7px] left-[11.25px]" />
                             </a>
                             <a className="basis-1/3"
-                            href="https://www.youtube.com/">
-                                <BgYoutube className="absolute"/>
-                                <IconYoutube className="relative top-[8.5px] left-[7.75px]"/>
+                                href="https://www.instagram.com/">
+                                <BgInstagram className="absolute" />
+                                <IconInstagram className="relative top-[6.5px] left-[7.25px]" />
+                            </a>
+                            <a className="basis-1/3"
+                                href="https://www.youtube.com/">
+                                <BgYoutube className="absolute" />
+                                <IconYoutube className="relative top-[8.5px] left-[7.75px]" />
                             </a>
                         </div>
                     </div>
                 </div>
-                <div className="basis-5/12 sm:basis-1/5 mt-4 sm:mt-[50px]"> 
+                <div className="basis-5/12 sm:basis-1/5 mt-4 sm:mt-[50px]">
                     <p className="text-b12 sm:text-b7 pl-[20px] my-[16.5px]">Giới thiệu</p>
                     <ul className="list-none pl-[20px]">
                         <li className="my-[16.5px] sm:my-[30px]">
@@ -77,10 +77,14 @@ export default function Footer({props}) {
                 </div>
                 <div className="basis-7/12 sm:basis-5/12 mt-4 sm:mt-[50px]">
                     <p className="text-b12 sm:text-b7 pl-[20px] my-[16.5px]">Thông tin liên hệ</p>
-                        <ul className="list-none pl-[20px]">
-                            <li className="my-[16.5px] sm:my-[30px] text-b13 sm:text-b8 ">Số điện thoại: <br className="sm:hidden"/><span className="text-orange" >083 913 2695</span></li>
-                            <li className="my-[16.5px] sm:my-[30px] text-b13 sm:text-b8 ">Địa chỉ: <br className="sm:hidden"/><span className="text-orange" >Hàn Thuyên, Thủ Đức, TP HCM</span></li>
-                        </ul>
+                    <ul className="list-none pl-[20px]">
+                        <li className="my-[16.5px] sm:my-[30px] text-b13 sm:text-b8 ">Số điện thoại: <br className="sm:hidden" /><span className="text-orange" >083 913 2695</span></li>
+                        <li className="my-[16.5px] sm:my-[30px] text-b13 sm:text-b8 ">Địa chỉ: <br className="sm:hidden" /><span className="text-orange" >Hàn Thuyên, Thủ Đức, TP HCM</span></li>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8065.893357110389!2d106.80551837713952!3d10.877217811230125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2sUniversity%20of%20Information%20Technology%20-%20VNUHCM!5e0!3m2!1sen!2s!4v1670040218490!5m2!1sen!2s"
+                            loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                            className="border border-solid rounded-lg w-96"
+                        ></iframe>
+                    </ul>
                 </div>
             </div>
         </footer>
