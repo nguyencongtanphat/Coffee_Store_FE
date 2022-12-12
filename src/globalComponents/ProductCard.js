b1b8ba7a6e4b082e4ebf0ed68e46741c3eeb4d35
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2022-12-12 10:41:52 +0700

    fix object-fit img card

--- src/globalComponents/ProductCard.js
+++ src/globalComponents/ProductCard.js
@@ -49,7 +49,7 @@ const ProductCard = (props) => {
         <img
           src={props.avtSrc}
           alt="This is a product avatar"
-          className={`${styles.productImg} w-[8.5rem] my-4 mx-2 max-h-28
+          className={`${styles.productImg} object-cover w-[8.5rem] my-4 mx-2 max-h-28
                     lg:m-4 lg:max-h-48 lg:max-w-60 lg:w-60 rounded-2xl`}
         />
       </div>
